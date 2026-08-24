17728db34a4ca55e7b773114588c08c9ec09f616
shakil-solaiman <solaimanshakil56@gmail.com>
2026-08-25 02:36:40 +0600

    All about variable

--- variable.js
+++ variable.js
@@ -1,1 +1,27 @@
 console.log("This is the first program of JS.")
+
+
+//var : Variable can be re-declared & updated. A global scope variable.
+//let : Variable cannot be re-declared but can be updated. A block scope variable.
+//const : Variable cannot be re-declared or updated. A block scope variable.
+
+// Types of Data types: SSBBNNUO ---  Symbol, String, Boolean, BigInt, Number, Null, Undefined, Object
+
+var number = 500;
+console.log(number);
+
+let name = "Solaiman Shakil";
+name = "Karim Ahmed";
+
+console.log(name);
+
+const age = 25;
+//age = 28; 
+
+console.log(age);
+
+const isAdult = true;
+
+console.log ("Types of variable: ", typeof(name), typeof(age), typeof(isAdult));
+
+
